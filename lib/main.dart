78e17392b42c960088667e982974f34c83e62562
yeshuaaguilar <yeshunator.12@gmail.com>
2023-12-09 17:36:01 -0500

    correcion de navigator en main y creando rutas

--- lib/main.dart
+++ lib/main.dart
@@ -12,75 +12,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home',
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Image.asset(
-              'assets/img/hambur.jpg',
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            Transform.translate(
-              offset: const Offset(110, 100),
-              child: CircleAvatar(
-                radius: 100,
-                backgroundColor: Colors.transparent,
-                child: ClipOval(
-                  child: Image.asset(
-                    'assets/img/logo_puntosano.png',
-                    fit: BoxFit.cover,
-                  ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/login': (context) => const Login(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/img/hambur.jpg',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Transform.translate(
+            offset: const Offset(110, 100),
+            child: CircleAvatar(
+              radius: 100,
+              backgroundColor: Colors.transparent,
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/img/logo_puntosano.png',
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
-            Positioned(
-              bottom: 150,
-              left: 60,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Login()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  minimumSize: const Size(280, 50),
-                ),
-                child: const Text(
-                  'Log In',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
+          ),
+          Positioned(
+            bottom: 150,
+            left: 60,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: const Size(280, 50),
+              ),
+              child: const Text(
+                'Log In',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ),
-            const Positioned(
-              bottom: 100,
-              left: 70,
-              child: Text(
-                '¿No tienes una cuenta?',
-                style: TextStyle(fontSize: 17, color: Colors.black),
-              ),
+          ),
+          const Positioned(
+            bottom: 100,
+            left: 70,
+            child: Text(
+              '¿No tienes una cuenta?',
+              style: TextStyle(fontSize: 17, color: Colors.black),
             ),
-            Positioned(
-              bottom: 100,
-              right: 70,
-              child: GestureDetector(
-                onTap: () {
-                  print('Texto presionado');
-                },
-                child: const Text(
-                  'Registrarse',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
-                  ),
+          ),
+          Positioned(
+            bottom: 100,
+            right: 70,
+            child: GestureDetector(
+              onTap: () {
+                print('Texto presionado');
+              },
+              child: const Text(
+                'Registrarse',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
