e902d5556b3aeda32875052dead350227f350d2c
yeshuaaguilar <yeshunator.12@gmail.com>
2023-12-10 19:31:43 -0500

    registre 3 completado

--- lib/registre3.dart
+++ lib/registre3.dart
@@ -11,6 +11,8 @@ class _Registre3State extends State<Registre3> {
   String pregunta1Selecionado = '';
   String pregunta2Selecionado = '';
   String pregunta3Selecionado = '';
+  String pregunta4Selecionado = '';
+  String pregunta5Selecionado = '';
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +26,14 @@ class _Registre3State extends State<Registre3> {
             fit: BoxFit.cover,
           ),
           Positioned(
-            left: 20,
-            bottom: 100,
+            left: 15,
+            bottom: 60,
             child: Center(
               child: Padding(
                 padding: const EdgeInsets.all(0.0),
                 child: Container(
-                  width: 370,
-                  height: 600,
+                  width: 380,
+                  height: 680,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20),
@@ -54,21 +56,21 @@ class _Registre3State extends State<Registre3> {
                           const Text(
                             '¿Has experimentado fatiga o falta de energia ultimamente?',
                             style: TextStyle(
-                              fontSize: 16,
+                              fontSize: 15,
                               fontWeight: FontWeight.bold,
                             ),
                             textAlign: TextAlign.center,
                           ),
                           Wrap(
                             alignment: WrapAlignment.start,
-                            spacing: 8.0,
-                            runSpacing: 8.0,
+                            spacing: -30.0,
+                            runSpacing: 1.0,
                             children: [
                               Container(
                                 width: 110,
                                 child: RadioListTile(
                                   title: Text('si'),
-                                  value: 'opcion1',
+                                  value: 'si',
                                   groupValue: pregunta1Selecionado,
                                   onChanged: (value) {
                                     setState(() {
@@ -81,7 +83,7 @@ class _Registre3State extends State<Registre3> {
                                 width: 220,
                                 child: RadioListTile(
                                   title: Text('ocasionalmente'),
-                                  value: 'opcion2',
+                                  value: 'ocasionalmente',
                                   groupValue: pregunta1Selecionado,
                                   onChanged: (value) {
                                     setState(() {
@@ -94,7 +96,7 @@ class _Registre3State extends State<Registre3> {
                                 width: 120,
                                 child: RadioListTile(
                                   title: Text('no'),
-                                  value: 'opcion3',
+                                  value: 'no',
                                   groupValue: pregunta1Selecionado,
                                   onChanged: (value) {
                                     setState(() {
@@ -107,40 +109,27 @@ class _Registre3State extends State<Registre3> {
                           ),
                           //pregunta 2
                           const Text(
-                            '¿Has experimentado fatiga o falta de energia ultimamente?',
+                            '¿Notas algun cambio en tu nivel de energia en comparacion con periodos anteriores?',
                             style: TextStyle(
-                              fontSize: 16,
+                              fontSize: 15,
                               fontWeight: FontWeight.bold,
                             ),
                             textAlign: TextAlign.center,
                           ),
                           Wrap(
                             alignment: WrapAlignment.start,
-                            spacing: 8.0,
-                            runSpacing: 8.0,
+                            spacing: -10.0,
+                            runSpacing: 1.0,
                             children: [
                               Container(
                                 width: 110,
                                 child: RadioListTile(
                                   title: Text('si'),
-                                  value: 'opcion1',
-                                  groupValue: pregunta1Selecionado,
+                                  value: 'si',
+                                  groupValue: pregunta2Selecionado,
                                   onChanged: (value) {
                                     setState(() {
-                                      pregunta1Selecionado = value.toString();
-                                    });
-                                  },
-                                ),
-                              ),
-                              Container(
-                                width: 220,
-                                child: RadioListTile(
-                                  title: Text('ocasionalmente'),
-                                  value: 'opcion2',
-                                  groupValue: pregunta1Selecionado,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      pregunta1Selecionado = value.toString();
+                                      pregunta2Selecionado = value.toString();
                                     });
                                   },
                                 ),
@@ -149,11 +138,176 @@ class _Registre3State extends State<Registre3> {
                                 width: 120,
                                 child: RadioListTile(
                                   title: Text('no'),
-                                  value: 'opcion3',
-                                  groupValue: pregunta1Selecionado,
+                                  value: 'no',
+                                  groupValue: pregunta2Selecionado,
                                   onChanged: (value) {
                                     setState(() {
-                                      pregunta1Selecionado = value.toString();
+                                      pregunta2Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          //pregunta 3
+                          const Text(
+                            '¿Encuentras dificultades para realizar actividades diarias, como subir escaleras, caminar distancias cortas, o realizar tareas cotidianas?',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Wrap(
+                            alignment: WrapAlignment.start,
+                            spacing: -10.0,
+                            runSpacing: 1.0,
+                            children: [
+                              Container(
+                                width: 110,
+                                child: RadioListTile(
+                                  title: Text('si'),
+                                  value: 'si',
+                                  groupValue: pregunta3Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta3Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 170,
+                                child: RadioListTile(
+                                  title: Text('a veces'),
+                                  value: 'aveces',
+                                  groupValue: pregunta3Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta3Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 120,
+                                child: RadioListTile(
+                                  title: Text('no'),
+                                  value: 'no',
+                                  groupValue: pregunta3Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta3Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          //pregunta 4
+                          const Text(
+                            '¿Experimentas dolor en las articulaciones o los musculos, especialmente en areas como las rodillas, la espalda o las caderas?',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Wrap(
+                            alignment: WrapAlignment.start,
+                            spacing: -10.0,
+                            runSpacing: 1.0,
+                            children: [
+                              Container(
+                                width: 110,
+                                child: RadioListTile(
+                                  title: Text('si'),
+                                  value: 'si',
+                                  groupValue: pregunta4Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta4Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 160,
+                                child: RadioListTile(
+                                  title: Text('a veces'),
+                                  value: 'aveces',
+                                  groupValue: pregunta4Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta4Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 120,
+                                child: RadioListTile(
+                                  title: Text('no'),
+                                  value: 'no',
+                                  groupValue: pregunta4Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta4Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          //pregunta 5
+                          const Text(
+                            '¿Tienes conocimiento de tu presion arterial o has experimentado problemas cardiacos en el pasado?',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Wrap(
+                            alignment: WrapAlignment.start,
+                            spacing: -10.0,
+                            runSpacing: 1.0,
+                            children: [
+                              Container(
+                                width: 110,
+                                child: RadioListTile(
+                                  title: Text('si'),
+                                  value: 'si',
+                                  groupValue: pregunta5Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta5Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 160,
+                                child: RadioListTile(
+                                  title: Text('a veces'),
+                                  value: 'aveces',
+                                  groupValue: pregunta5Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta5Selecionado = value.toString();
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 120,
+                                child: RadioListTile(
+                                  title: Text('no'),
+                                  value: 'no',
+                                  groupValue: pregunta5Selecionado,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      pregunta5Selecionado = value.toString();
                                     });
                                   },
                                 ),
