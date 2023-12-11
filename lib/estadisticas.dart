e1b92f017283e57e4bc3a89a82decb0bb24956c2
yeshuaaguilar <yeshunator.12@gmail.com>
2023-12-11 11:50:42 -0500

    estadistica avanses

--- lib/estadisticas.dart
+++ lib/estadisticas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class Estadistica extends StatelessWidget {
   const Estadistica({
@@ -8,25 +9,81 @@ class Estadistica extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //barra de arriba
       appBar: AppBar(
         title: Text('Diagnóstico'),
+        leading: Container(),
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Center(
-        child: Text('Hola Mundo'),
+        child: Column(
+          children: <Widget>[
+            Text('Gráfico de Barras'),
+            Container(
+              height: 200,
+              child: BarChart(
+                BarChartData(
+                  barGroups: [
+                    BarChartGroupData(
+                      x: 0,
+                      barRods: [
+                        BarChartRodData(
+                          toY: 40,
+                          color: Colors.blue,
+                        ),
+                      ],
+                    ),
+                    BarChartGroupData(
+                      x: 1,
+                      barRods: [
+                        BarChartRodData(
+                          toY: 70,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                    BarChartGroupData(
+                      x: 2,
+                      barRods: [
+                        BarChartRodData(
+                          toY: 30,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ],
+                  titlesData: FlTitlesData(
+                    show: true,
+                    bottomTitles: SideTitles(
+                      showTitles: false,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+      //barra de navegacion inferior
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
+            icon: Icon(Icons.sports_handball),
+            label: 'Actividades',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Buscar',
+            icon: Icon(Icons.stacked_bar_chart),
+            label: 'Estadisticas',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
+            icon: Icon(Icons.restaurant),
+            label: 'comida',
           ),
         ],
       ),
