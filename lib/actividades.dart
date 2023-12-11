8f7a521fe72e53e50d12cb1e940985774daf6235
yeshuaaguilar <yeshunator.12@gmail.com>
2023-12-11 16:47:50 -0500

    barra de progreso avanses 3

--- lib/actividades.dart
+++ lib/actividades.dart
@@ -2,15 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-class ActividadesApp extends StatelessWidget {
+class ActividadesApp extends StatefulWidget {
+  @override
+  _ActividadesAppState createState() => _ActividadesAppState();
+}
+
+class _ActividadesAppState extends State<ActividadesApp> {
+  double progressValue = 0.0;
+  List<bool> completedTasks = [false, false, false];
+
+  @override
+  void setState(VoidCallback fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  void updateProgress() {
+    int completedCount = completedTasks.where((completed) => completed).length;
+    progressValue = completedCount / completedTasks.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    double progressValue = 0.5;
     initializeDateFormatting('es');
-
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('dd MMMM', 'es').format(now);
-
     return MaterialApp(
       home: Scaffold(
         body: Center(
@@ -26,16 +43,13 @@ class ActividadesApp extends StatelessWidget {
               ),
               SizedBox(height: 16),
               Container(
-                width:
-                    300, // Ajusta el ancho del contenedor según tus necesidades
-                height:
-                    20, // Ajusta la altura del contenedor según tus necesidades
+                width: 300,
+                height: 20,
                 child: Stack(
                   children: [
                     LinearProgressIndicator(
                       value: progressValue,
-                      backgroundColor: Color.fromARGB(255, 201, 201,
-                          201), // Cambia el color de fondo de la barra de progreso
+                      backgroundColor: Color.fromARGB(255, 201, 201, 201),
                       minHeight: 20,
                     ),
                     Align(
@@ -43,15 +57,14 @@ class ActividadesApp extends StatelessWidget {
                       child: FractionallySizedBox(
                         widthFactor: progressValue,
                         child: Container(
-                          color: Colors
-                              .blue, // Cambia el color de la parte llena de la barra de progreso
+                          color: Colors.blue,
                         ),
                       ),
                     ),
                     Align(
                       alignment: Alignment.center,
                       child: Text(
-                        '${(progressValue * 100).toStringAsFixed(0)}%', // Muestra el porcentaje basado en el valor actual
+                        '${(progressValue * 100).toStringAsFixed(0)}%',
                         style: TextStyle(
                           color: const Color.fromARGB(255, 0, 0, 0),
                           fontWeight: FontWeight.bold,
@@ -61,6 +74,56 @@ class ActividadesApp extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 300,
+                height: 300,
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: Text('Tarea 1'),
+                      subtitle: Text('Esta es la descripción de la tarea 1'),
+                      trailing: Checkbox(
+                        value: completedTasks[0]!,
+                        onChanged: (value) {
+                          setState(() {
+                            completedTasks[0] = value!;
+                            updateProgress();
+                          });
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Tarea 2'),
+                      subtitle: Text('Esta es la descripción de la tarea 2'),
+                      trailing: Checkbox(
+                        value: completedTasks[1]!,
+                        onChanged: (value) {
+                          setState(() {
+                            completedTasks[1] = value!;
+                            updateProgress();
+                          });
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Tarea 3'),
+                      subtitle: Text('Esta es la descripción de la tarea 3'),
+                      trailing: Checkbox(
+                        value: completedTasks[2]!,
+                        onChanged: (value) {
+                          setState(() {
+                            completedTasks[2] = value!;
+                            updateProgress();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
