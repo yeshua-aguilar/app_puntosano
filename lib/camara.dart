4ae317e4ec3e9fa0d50e855ecb6cd8e19853d8a8
yeshuaaguilar <yeshunator.12@gmail.com>
2023-12-10 20:17:09 -0500

    camara v2

--- lib/camara.dart
+++ lib/camara.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-class Camara extends StatelessWidget {
+class Camara extends StatefulWidget {
   const Camara({
     Key? key,
   }) : super(key: key);
 
   @override
+  _CamaraState createState() => _CamaraState();
+}
+
+class _CamaraState extends State<Camara> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeControllerFuture = _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    _controller = CameraController(cameras.first, ResolutionPreset.high);
+    await _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return _buildCameraPreview();
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildCameraPreview() {
     return Scaffold(
       body: Stack(
         children: [
-          Text('hola mundo'),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: CameraPreview(_controller),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: FloatingActionButton(
+              onPressed: () async {
+                final image = await _controller.takePicture();
+                // TODO: Save the image to the gallery.
+              },
+              child: const Icon(Icons.camera),
+            ),
+          ),
         ],
       ),
     );
